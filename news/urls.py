511403eef7039485528392926da8f03419523de1
Jard-z <vitalii0595@gmail.com>
2021-05-24 16:17:30 +0300

    Add inclusion_tag show_categories

--- news/urls.py
+++ news/urls.py
@@ -4,5 +4,5 @@ from .views import *
 
 urlpatterns = [
     path('', index, name='home'),
-    path('category/<int:category_id>', get_categories, name='categories')
+    path('category/<int:category_id>', get_categories, name='category')
 ]
