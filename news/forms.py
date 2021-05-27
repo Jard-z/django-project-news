6682117ad97226740baf3d93bf7bb9fbdb59aaf2
Jard-z <vitalii0595@gmail.com>
2021-05-27 18:57:55 +0300

    Create ModelForm

--- news/forms.py
+++ news/forms.py
@@ -1,10 +1,13 @@
 from django import forms
-from .models import Category
+from .models import News
 
 
-class NewsForm(forms.Form):
-    title = forms.CharField(max_length=100)
-    content = forms.CharField()
-    is_published = forms.BooleanField()
-    category = forms.ModelChoiceField(queryset=Category.objects.all())
-
+class NewsForm(forms.ModelForm):
+    class Meta:
+        model = News
+        fields = ['title', 'content', 'is_published', 'category']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'content': forms.Textarea(attrs={'class': 'form-control', 'rows': 5}),
+            'category': forms.Select(attrs={'class': 'form-control'}),
+        }
