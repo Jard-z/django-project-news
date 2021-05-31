0ee23e9148e26153c2d550e39da035e09fcb8066
Jard-z <vitalii0595@gmail.com>
2021-05-31 22:03:04 +0300

    Counter news

--- news/views.py
+++ news/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import ListView, DetailView, CreateView
 
+
 from .models import *
 from .forms import *
 
